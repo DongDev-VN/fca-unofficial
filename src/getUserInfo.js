f025d2d5440b2c0008606d1648578793055dfc4b
Phạm Minh Đồng <dongdz.user@gmail.com>
2025-04-24 23:16:30 +0700

    hot fix version to v1.0.3

--- src/getUserInfo.js
+++ src/getUserInfo.js
@@ -1,68 +1,70 @@
 "use strict";
 
-const utils = require("../utils");
-const log = require("npmlog");
-
-function formatData(data) {
-	const retObj = {};
-	for (const prop in data) {
-		if (data.hasOwnProperty(prop)) {
-			const innerObj = data[prop];
-			retObj[prop] = {
-				name: innerObj.name,
-				firstName: innerObj.firstName,
-				vanity: innerObj.vanity,
-				thumbSrc: innerObj.thumbSrc,
-				profileUrl: innerObj.uri,
-				gender: innerObj.gender,
-				type: innerObj.type,
-				isFriend: innerObj.is_friend,
-				isBirthday: !!innerObj.is_birthday,
-				searchTokens: innerObj.searchTokens,
-				alternateName: innerObj.alternateName
-			};
-		}
-	}
-	return retObj;
-}
+var utils = require("../utils");
+var log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
-	return function getUserInfo(id, callback) {
-		let resolveFunc = function () { };
-		let rejectFunc = function () { };
-		const returnPromise = new Promise(function (resolve, reject) {
+	function formatData(data) {
+		const retObj = {};
+		for (const actor of data.messaging_actors || []) {
+			retObj[actor.id] = {
+				name: actor.name,
+				firstName: actor.short_name || null,
+				vanity: actor.username || null,
+				thumbSrc: actor.big_image_src?.uri || null,
+				profileUrl: actor.url || null,
+				gender: actor.gender || null,
+				type: actor.__typename || null,
+				isFriend: actor.is_viewer_friend || false,
+				isMessengerUser: actor.is_messenger_user || false,
+				isMessageBlockedByViewer: actor.is_message_blocked_by_viewer || false,
+				workInfo: actor.work_info || null,
+				messengerStatus: actor.messenger_account_status_category || null
+			};
+		}
+		return retObj;
+	}
+	return function getUserInfoGraphQL(id, callback) {
+		let resolveFunc, rejectFunc;
+		const returnPromise = new Promise((resolve, reject) => {
 			resolveFunc = resolve;
 			rejectFunc = reject;
 		});
-		if (!callback) {
-			callback = function (err, friendList) {
-				if (err) {
-					return rejectFunc(err);
-				}
-				resolveFunc(friendList);
+		if (typeof callback !== "function") {
+			callback = (err, data) => {
+				if (err) return rejectFunc(err);
+				resolveFunc(data);
 			};
 		}
-		if (utils.getType(id) !== "Array") {
-			id = [id];
-		}
-		const form = {};
-		id.map(function (v, i) {
-			form["ids[" + i + "]"] = v;
-		});
-		defaultFuncs
-			.post("https://www.facebook.com/chat/user_info/", ctx.jar, form)
-			.then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-			.then(function (resData) {
-				if (resData.error) {
-					throw resData;
+		const form = {
+			queries: JSON.stringify({
+				o0: {
+					doc_id: "5009315269112105",
+					query_params: {
+						ids: [id]
+					}
 				}
-				return callback(null, formatData(resData.payload.profiles));
+			}),
+			batch_name: "MessengerParticipantsFetcher"
+		};
+		defaultFuncs
+			.post("https://www.facebook.com/api/graphqlbatch/", ctx.jar, form)
+			.then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+			.then(resData => {
+				if (!resData || resData.error) {
+					throw resData?.error || new Error("Unknown error from GraphQL API");
+				}
+				const result = resData[0]?.o0?.data || null;
+				if (!result) {
+					console.error("getUserInfo", "Không nhận được dữ liệu hợp lệ!");
+					return callback(new Error("Invalid data received"));
+				}
+				callback(null, formatData(result));
 			})
-			.catch(function (err) {
-				log.error("getUserInfo", err);
-				return callback(err);
+			.catch(err => {
+				log.error("getUserInfoV4GraphQL", "Lỗi: Có thể do bạn spam quá nhiều, hãy thử lại!");
+				callback(err);
 			});
-
 		return returnPromise;
 	};
 };
